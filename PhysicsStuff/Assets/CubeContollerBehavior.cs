18b99fb6bf0da442e427ad5c9f1a06e024b5fadd
Jax Mossman <Jax.mossman@gmail.com>
2020-09-24 10:19:16 -0500

    Fixed Repo Issues

---
+++ PhysicsStuff/Assets/CubeContollerBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeContollerBehavior : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    Ray ray;
+    RaycastHit hit;
+    private Vector3 velocity;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(ray, out hit)) {
+            Transform objectHit = hit.transform;
+            
+            text.text = hit.collider.gameObject.name;
+            // Do something with the object that was hit by the raycast.
+        }
+            
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       
+        
+        // 以下、キャラクターの移動処理
+        velocity = new Vector3(ray.direction.x, ray.direction.y,0);
+        velocity.z = 0;
+        velocity *= speed;
+
+        velocity = transform.TransformDirection(velocity);
+        transform.localPosition += velocity * Time.fixedDeltaTime;
+
+    }
+}
---
+++ PhysicsStuff/Assets/KatamariChanBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KatamariChanBehavior : MonoBehaviour
+{
+    [HideInInspector]
+    public int Score;
+    // Start is called before the first frame update
+
+    private Rigidbody rb;
+    private List<SpringJoint> removals;
+    List<SpringJoint> springJoints = new List<SpringJoint>();
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        //break connections
+        if (Input.GetAxis("Jump") != 0)
+        {
+
+            foreach (SpringJoint springJoint in springJoints)
+            {
+
+                springJoint.breakForce = 0;
+                springJoint.breakTorque = 0;
+
+            }
+            springJoints.Clear();
+
+        }
+        if (Input.GetAxis("Fire1") != 0)
+        {
+            rb.isKinematic = !rb.isKinematic;
+        }
+    }
+
+    //attach items when colliding 
+    private void OnTriggerEnter(Collider other)
+    { 
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+
+            other.gameObject.tag = "Collected";
+            Score++;
+
+
+            other.gameObject.AddComponent<SpringJoint>();
+            SpringJoint spring = other.gameObject.GetComponent<SpringJoint>();
+            spring.connectedBody = this.rb;
+            spring.tolerance = 1;
+            spring.minDistance = 0;
+
+            spring.maxDistance = 1;
+            spring.spring = 10000;
+            spring.damper = 10000000000;
+            springJoints.Add(spring);
+        }
+    }
+}
